3bbae9a7e4716c9dd954a2a4d873f58ecffe8e02
Marina <marinavila.mv@gmail.com>
2023-03-07 16:55:09 +0100

    css con tailwinds

--- src/componentes/header/MainHeader.jsx
+++ src/componentes/header/MainHeader.jsx
@@ -1,23 +1,26 @@
 import LogoSneakers from "@/assets/images/logo.svg";
 import AvatarImagen from "@/assets/images/image-avatar.png";
-import MenuIcon from "@/componentes/icons/MenuIcon.jsx";
+import MenuIcon from "@/componentes/icons/MenuIcon";
+import CartIcon from "@/componentes/icons/CartIcon";
 
 const MainHeader = () => {
   return (
-    <header>
-      <button>
+    <header className="flex">
+      <button className="md:hidden">
         <MenuIcon />
       </button>
-      <img src={LogoSneakers} alt="Logo" />
-      <nav>
+      <img src={LogoSneakers} alt="Logo sneakers" className="mr-auto md:mr-0" />
+      <nav className="hidden md:block md:mr-auto">
         <a href="#">Collections</a>
         <a href="#">Men</a>
         <a href="#">Women</a>
         <a href="#">About</a>
         <a href="#">Contact</a>
       </nav>
-      <div>
-        <button>cart</button>
+      <div className="flex gap-4">
+        <button>
+          <CartIcon />
+        </button>
         <img src={AvatarImagen} alt="avatar" />
       </div>
     </header>
