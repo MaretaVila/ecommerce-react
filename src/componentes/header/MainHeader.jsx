82fb491298d50ad3f8e4e8dbad474a86e70f3044
Marina <marinavila.mv@gmail.com>
2023-03-28 14:35:38 +0200

    creation of componentes

--- src/componentes/header/MainHeader.jsx
+++ src/componentes/header/MainHeader.jsx
@@ -4,13 +4,21 @@ import MenuIcon from "@/componentes/icons/MenuIcon";
 import CartIcon from "@/componentes/icons/CartIcon";
 
 const MainHeader = () => {
+  const handleOpenMenu = () => {
+    console.log("click");
+  };
+
   return (
-    <header className="flex">
-      <button className="md:hidden">
+    <header className="container mx-auto flex items-center gap-4 px-4 py-8 bg-gray-200">
+      <button className="md:hidden" onClick={handleOpenMenu}>
         <MenuIcon />
       </button>
-      <img src={LogoSneakers} alt="Logo sneakers" className="mr-auto md:mr-0" />
-      <nav className="hidden md:block md:mr-auto">
+      <img
+        src={LogoSneakers}
+        alt="Logo sneakers"
+        className="mr-auto mb-1 h-5 md:mr-0"
+      />
+      <nav className="absolute bg-gray-400 flex flex-col md:mr-auto md:flex md:flex-row md:gap-8">
         <a href="#">Collections</a>
         <a href="#">Men</a>
         <a href="#">Women</a>
@@ -21,7 +29,7 @@ const MainHeader = () => {
         <button>
           <CartIcon />
         </button>
-        <img src={AvatarImagen} alt="avatar" />
+        <img src={AvatarImagen} alt="avatar" className="w-10" />
       </div>
     </header>
   );
