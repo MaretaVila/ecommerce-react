08032814f7ca690ebd6fe56bf74e71afd0deba57
Marina <marinavila.mv@gmail.com>
2023-03-29 14:37:06 +0200

    configuration of colors in tailwind

--- src/componentes/header/NavLinkHeader.jsx
+++ src/componentes/header/NavLinkHeader.jsx
@@ -1,3 +1,8 @@
 export default ({ text }) => {
-  return <a href="#">{text}</a>;
+  return (
+    <a href="#" className="py-8 relative group">
+      <span className="group-hover:text-gray-700">{text}</span>
+      <span className="absolute bottom-0 block w-full scale-x-0 transition-all duration-500 h-1 group-hover:scale-x-100 group-hover:bg-orange-primary"></span>
+    </a>
+  );
 };
