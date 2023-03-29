f72d4a3daa5c7e1869f900932b3f15ae7fa2cf0c
Marina <marinavila.mv@gmail.com>
2023-03-29 12:33:03 +0200

    creation header con tailwind

--- src/componentes/header/MainHeader.jsx
+++ src/componentes/header/MainHeader.jsx
@@ -2,36 +2,57 @@ import LogoSneakers from "@/assets/images/logo.svg";
 import AvatarImagen from "@/assets/images/image-avatar.png";
 import MenuIcon from "@/componentes/icons/MenuIcon";
 import CartIcon from "@/componentes/icons/CartIcon";
+import CloseIcon from "@/componentes/icons/CloseIcon";
+import { useState } from "react";
+import NavLinkHeader from "@/componentes/header/NavLinkHeader";
 
 const MainHeader = () => {
+  const [navClass, setNavClass] = useState(
+    "hidden font-bold md:static md:mr-auto md:h-auto md:flex md:flex-row md:gap-4 md:p-0"
+  );
+
   const handleOpenMenu = () => {
-    console.log("click");
+    setNavClass(
+      "absolute font-bold top-0 left-0 h-full p-8 gap-y-5 w-4/5 flex flex-col bg-white md:static md:mr-auto md:h-auto md:flex md:flex-row md:gap-4 md:p-0"
+    );
+  };
+
+  const handleCloseMenu = () => {
+    setNavClass(
+      "hidden font-bold md:static md:mr-auto md:h-auto md:flex md:flex-row md:gap-4 md:p-0"
+    );
   };
 
   return (
-    <header className="container mx-auto flex items-center gap-4 px-4 py-8 bg-gray-200">
-      <button className="md:hidden" onClick={handleOpenMenu}>
-        <MenuIcon />
-      </button>
-      <img
-        src={LogoSneakers}
-        alt="Logo sneakers"
-        className="mr-auto mb-1 h-5 md:mr-0"
-      />
-      <nav className="absolute bg-gray-400 flex flex-col md:mr-auto md:flex md:flex-row md:gap-8">
-        <a href="#">Collections</a>
-        <a href="#">Men</a>
-        <a href="#">Women</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
-      </nav>
-      <div className="flex gap-4">
-        <button>
-          <CartIcon />
+    <>
+      <header className="container mx-auto flex items-center gap-4 py-8">
+        <button className="md:hidden" onClick={handleOpenMenu}>
+          <MenuIcon />
         </button>
-        <img src={AvatarImagen} alt="avatar" className="w-10" />
-      </div>
-    </header>
+        <img
+          src={LogoSneakers}
+          alt="Logo sneakers"
+          className="mr-auto mb-1 h-5 md:mr-0"
+        />
+        <nav className={navClass}>
+          <button className="mb-8 md:hidden" onClick={handleCloseMenu}>
+            <CloseIcon />
+          </button>
+          <NavLinkHeader text="Collections" />
+          <NavLinkHeader text="Men" />
+          <NavLinkHeader text="Women" />
+          <NavLinkHeader text="About" />
+          <NavLinkHeader text="Contact" />
+        </nav>
+        <div className="flex gap-4">
+          <button>
+            <CartIcon />
+          </button>
+          <img src={AvatarImagen} alt="avatar" className="w-10" />
+        </div>
+      </header>
+      <span className="container mx-auto hidenn h-[1px] bg-gray-200 w-full md:block"></span>
+    </>
   );
 };
 
---
+++ src/componentes/header/NavLinkHeader.jsx
@@ -0,0 +1,3 @@
+export default ({ text }) => {
+  return <a href="#">{text}</a>;
+};
