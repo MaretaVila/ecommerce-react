1503a66f3a70b0997e46aaf64241bf06f744a226
Marina <marinavila.mv@gmail.com>
2023-05-10 12:28:19 +0200

    hover en modal y seleccion de imagen

--- src/componentes/products/col-images/SlideProducts.jsx
+++ src/componentes/products/col-images/SlideProducts.jsx
@@ -1,6 +1,7 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NextIcon from "@/componentes/icons/NextIcon";
 import PreviousIcon from "@/componentes/icons/PreviousIcon";
+import CloseIcon from "@/componentes/icons/CloseIcon";
 
 export default ({
   ARRAY_IMGS = [],
@@ -12,6 +13,11 @@ export default ({
 }) => {
   const btnSlider = useRef(null);
   const [index, setIndex] = useState(0);
+
+  //useEffect siempre esta pendiente de algo, en este caso de Open Modal
+  // useEffect(() => {
+  //   isOpenModal && btnSlider.current.classList.remove("md:hidden");
+  // }, [isOpenModal]);
 
   //reemplazo el if, utilizo un operador ternario para el return (?) y saco el ; y lo reemplazo por :
   const handleClickNext = () => {
@@ -26,7 +32,7 @@ export default ({
     <section {...props}>
       {isOpenModal && (
         <button onClick={handleCloseModal} className="md:col-span-4 text-right">
-          cerrar
+          <CloseIcon />
         </button>
       )}
       <div className="col-span-4 relative">
@@ -38,7 +44,9 @@ export default ({
         />
         <div
           ref={btnSlider}
-          className="absolute top-1/2 left-0 px-4 flex w-full -translate-y-1/2 justify-between"
+          className={`absolute top-1/2 left-0 px-4 flex w-full -translate-y-1/2 justify-between ${
+            !isOpenModal && "md:hidden"
+          }`}
         >
           <button
             className="w-10 h-10 rounded-full bg-white grid place-items-center"
@@ -54,14 +62,27 @@ export default ({
           </button>
         </div>
       </div>
-      {ARRAY_IMG_SMALL.map((smallImg) => (
-        <img
-          key={smallImg}
-          src={smallImg}
-          alt=""
-          className="hidden md:block md:rounded-md"
-          onClick={handleOpenModal}
-        />
+      {/* con la i accedo al indice que tiene la imagen y lo modifico con setIndex */}
+      {ARRAY_IMG_SMALL.map((smallImg, i) => (
+        <div
+          key={i}
+          onClick={() => {
+            setIndex(i);
+          }}
+          className="relative rounded-md overflow-hidden cursor-pointer"
+        >
+          <img
+            src={smallImg}
+            alt=""
+            className="hidden md:block md:rounded-md"
+            onClick={handleOpenModal}
+          />
+          <span
+            className={`absolute top-0 h-full w-full hover:bg-[rgba(255,255,255,0.5)] ${
+              i === index && "bg-[rgba(255,255,255,0.5)]"
+            } `}
+          ></span>
+        </div>
       ))}
     </section>
   );
