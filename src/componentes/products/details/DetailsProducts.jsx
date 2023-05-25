746d364f1a40ad8400ca6d24a1c01958f4875a6d
Marina <marinavila.mv@gmail.com>
2023-05-25 14:32:30 +0200

    add funcionality of total amount in the minicart

--- src/componentes/products/details/DetailsProducts.jsx
+++ src/componentes/products/details/DetailsProducts.jsx
@@ -1,9 +1,29 @@
 import CartIcon from "@/componentes/icons/CartIcon.jsx";
 import { useCartDetails } from "@/context/useCartDetails";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 export default ({ objectProduct }) => {
   const { addProducts } = useContext(useCartDetails);
+  const [count, setCount] = useState(0);
+
+  const decrementCount = () => {
+    if (count === 0) return;
+    setCount(count - 1);
+  };
+
+  const handleAddToCart = () => {
+    addProducts({
+      img: objectProduct.imagesSmall[0],
+      id: objectProduct.id,
+      discountPrice: (
+        objectProduct.price *
+        (1 - objectProduct.discount)
+      ).toFixed(2),
+      title: objectProduct.title,
+      quantity: count || 1,
+    });
+    setCount(0);
+  };
 
   return (
     <section className="container mx-auto px-4 md:px-4">
@@ -29,24 +49,23 @@ export default ({ objectProduct }) => {
 
       <div className="grid grid-cols-3 gap-4 font-bold md:grid-cols-[1fr_1.7fr]">
         <div className="col-span-3 flex items-baseline justify-between rounded-md bg-gray-200 py-2 px-5 pb-5 md:col-span-1">
-          <button className="text-3xl text-orange-primary">-</button>
-          <span className="text-xl">0</span>
-          <button className="text-3xl text-orange-primary">+</button>
+          <button
+            className="text-3xl text-orange-primary"
+            onClick={decrementCount}
+          >
+            -
+          </button>
+          <span className="text-xl">{count}</span>
+          <button
+            className="text-3xl text-orange-primary"
+            onClick={() => setCount(count + 1)}
+          >
+            +
+          </button>
         </div>
         <button
           className="col-span-3 flex items-center justify-center gap-x-3 rounded-md bg-orange-primary py-3 text-white transition-all hover:bg-orange-800 md:col-span-1"
-          onClick={() =>
-            addProducts({
-              img: objectProduct.imagesSmall[0],
-              id: objectProduct.id,
-              discountPrice: (
-                objectProduct.price *
-                (1 - objectProduct.discount)
-              ).toFixed(2),
-              title: objectProduct.title,
-              quantity: 1,
-            })
-          }
+          onClick={handleAddToCart}
         >
           <CartIcon className="fill-white" />
           <span>Add to cart</span>
